48c20c1f152e5c1812963ceca4dfa7b4d2087417
Scott Davis <stdavis@utah.gov>
2016-05-17 11:31:35 -0600

    npm updates and switch to eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     var jsFiles = 'src/app/**/*.js';
     var otherFiles = [
         'src/app/**/*.html',
@@ -6,16 +6,17 @@ module.exports = function(grunt) {
         'src/index.html'
     ];
     var gruntFile = 'GruntFile.js';
-    var internFile = 'tests/intern.js';
+    var internFile = 'ui-tests/intern.js';
     var packageFile = 'package.json';
-    var jshintFiles = [jsFiles, gruntFile, internFile, packageFile];
+    var eslintFiles = [jsFiles, gruntFile, internFile, packageFile];
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jasmine: {
             app: {
-                src: ['src/app/tests/jasmineTestBootstrap.js',
+                src: [
+                    'src/app/tests/jasmineTestBootstrap.js',
                     'src/dojo/dojo.js',
                     'src/app/run.js'
                 ],
@@ -26,19 +27,21 @@ module.exports = function(grunt) {
                 }
             }
         },
-        jshint: {
-            files: jshintFiles,
+        eslint: {
+            main: {
+                src: eslintFiles
+            },
             options: {
-                jshintrc: '.jshintrc'
+                configFile: '.eslintrc'
             }
         },
         watch: {
-            jshint: {
-                files: jshintFiles,
-                tasks: ['jshint']
+            eslint: {
+                files: eslintFiles,
+                tasks: ['eslint']
             },
             src: {
-                files: jshintFiles.concat(otherFiles),
+                files: eslintFiles.concat(otherFiles),
                 options: {
                     livereload: true
                 }
@@ -55,19 +58,21 @@ module.exports = function(grunt) {
             dev: {
                 options: {
                     runType: 'runner',
-                    config: 'intern.js'
+                    config: internFile
                 }
             }
         }
     });
 
     // Register tasks.
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+    require('load-grunt-tasks')(grunt);
     grunt.loadNpmTasks('intern');
 
     // Default task.
-    grunt.registerTask('default', ['jasmine:app:build', 'jshint', 'connect', 'watch']);
+    grunt.registerTask('default', [
+        'jasmine:app:build',
+        // 'eslint',
+        'connect',
+        'watch'
+    ]);
 };
