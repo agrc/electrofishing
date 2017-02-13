60ea722454bd648ff2814604deb1b840831fbda7
keatonwalker <keatwalker@gmail.com>
2017-02-13 15:21:26 -0700

    Stream length populated after verify #33

--- src/app/location/Location.js
+++ src/app/location/Location.js
@@ -187,6 +187,15 @@ define([
         addLineToMap: function (path) {
             // summary:
             //      adds the path to the map
+            var calculateDistance = function (line) {
+                var latLngArray = line._latlngs[0];
+                var distance = 0;
+                for (var i = 0; i < latLngArray.length - 1; i++) {
+                    distance += latLngArray[i].distanceTo(latLngArray[i + 1]);
+                }
+
+                return distance;
+            }
             console.log('app/location/Location:addLineToMap', arguments);
 
             this.verifyMapBtn.innerHTML = this.successfullyVerifiedMsg;
@@ -195,6 +204,8 @@ define([
             this.path = path;
             config.app.map.fitBounds(line.getBounds().pad(0.1));
             this.geometry = line;
+            var streamDistance = calculateDistance(this.geometry);
+            this.streamLengthTxt.value = streamDistance.toFixed();
         },
         getAdditionalCacheData: function () {
             // summary:
