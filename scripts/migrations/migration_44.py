348d267b546f0dca5f90aec01f3c41f823afc4b3
steveoh <sgourley@utah.gov>
2018-03-13 19:56:07 -0600

    create hard body domain
    refs #44

--- scripts/migrations/migration_44.py
+++ scripts/migrations/migration_44.py
@@ -10,20 +10,30 @@ import arcpy
 import domains
 
 arcpy.env.workspace = sde = join(dirname(__file__), 'local@electrofishing.sde')
+domain_list = arcpy.da.ListDomains(sde)
 
-print('creating new domain')
 domain_name = 'hard_body_part'
-arcpy.management.CreateDomain(sde, domain_name=domain_name, domain_description='the part of the body used', field_type='TEXT', domain_type='CODED')
+
+try:
+    domain = domains.get_domain_by_name(domain_name, domain_list)
+except Exception as e:
+    print(e)
+    print('creating new domain')
+    arcpy.management.CreateDomain(sde, domain_name=domain_name, domain_description='the part of the body used', field_type='TEXT', domain_type='CODED')
+    domain_list = arcpy.da.ListDomains(sde)
+    domain = domains.get_domain_by_name(domain_name, domain_list)
 
 print('adding values')
 new_domains = [
-    ('Scale',)
-    ('Spine',)
-    ('Ray',)
-    ('Cleithrum',)
-    ('Otolith',)
-    ('Other',)
+    'Scale',
+    'Spine',
+    'Ray',
+    'Cleithrum',
+    'Otolith',
+    'Other'
 ]
-domains.add_values_to_domain(domain_name, new_domains, sde)
+
+for code in new_domains:
+    domains.add_if_not_exists(domain, sde, code)
 
 print('done')
