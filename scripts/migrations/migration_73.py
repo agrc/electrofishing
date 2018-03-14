208817f97a8c03e74d28340e447d120a146d13ee
steveoh <sgourley@utah.gov>
2018-03-13 18:46:34 -0600

    move to migration and use domains.py

---
+++ scripts/migrations/migration_73.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+'''
+migration_73.py
+A module that adds a couple domain_list to tag type
+'''
+
+from os.path import dirname, join
+
+import arcpy
+import domains
+
+arcpy.env.workspace = sde = join(dirname(__file__), 'local@electrofishing.sde')
+domain_name = 'tag_types'
+
+print('getting domains')
+domain_list = arcpy.da.ListDomains(sde)
+
+print('getting specific domain')
+tag_domain = domains.get_domain_by_name(domain_name, domain_list)
+
+print('adding domains')
+domains.add_if_not_exists(tag_domain, sde, 'Need spray dye')
+domains.add_if_not_exists(tag_domain, sde, 'OTC')
+
+print('done')
