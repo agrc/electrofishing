aa86db90e862df7b454076857c25fbe7d8877196
steveoh <sgourley@utah.gov>
2018-06-07 18:11:20 -0600

    improve robustness and logging
    make keys into a list
    check that fields and insert items match or error and print the problem.
    print if the data is versioned or not because i forget

--- scripts/Scripts/NewCollectionEvent.py
+++ scripts/Scripts/NewCollectionEvent.py
@@ -11,8 +11,8 @@ GP Parameters:
 data(0): JSON String
 
 """
+import datetime
 import json
-from datetime import datetime
 
 import arcpy
 import settings
@@ -21,9 +21,21 @@ import settings
 def appendTableData(tableName, rows):
     arcpy.AddMessage('Adding row(s) to {}'.format(tableName))
 
-    with arcpy.da.InsertCursor(tableName, rows[0].keys()) as cursor:
+    fields = list(rows[0].keys())
+    field_set = set(fields)
+    with arcpy.da.InsertCursor(tableName, fields) as cursor:
         for row in rows:
-            cursor.insertRow([row[key] for key in row.keys()])
+            row_fields = list(row.keys())
+
+            uhoh = field_set ^ set(row_fields)
+
+            if len(uhoh) != 0:
+                message = 'fields do not match: {}'.format(','.join(uhoh))
+                arcpy.AddWarning(message)
+
+                raise Exception(message)
+
+            cursor.insertRow([row[key] for key in row_fields])
 
 
 def appendFeatureData(feature):
@@ -31,25 +43,30 @@ def appendFeatureData(feature):
 
     attributes = feature['attributes']
     time = None
+
     if ':' in attributes['EVENT_TIME']:
-        time = datetime.strptime(attributes['EVENT_TIME'], '%H:%M').time()
-    date = datetime.strptime(attributes[settings.EVENT_DATE], '%Y-%m-%d')
+        time = datetime.datetime.strptime(attributes['EVENT_TIME'], '%H:%M').time()
+
+    date = datetime.datetime.strptime(attributes[settings.EVENT_DATE], '%Y-%m-%d')
+
     if time is not None:
-        attributes[settings.EVENT_DATE] = datetime.combine(date, time)
+        attributes[settings.EVENT_DATE] = datetime.datetime.combine(date, time)
     else:
         attributes[settings.EVENT_DATE] = date
-    fields = attributes.keys() + ['SHAPE@JSON']
+
+    fields = list(attributes.keys()) + ['SHAPE@JSON']
     fields.remove('EVENT_TIME')
     attributes.pop('EVENT_TIME')
 
     with arcpy.da.InsertCursor(settings.SAMPLINGEVENTS, fields) as cursor:
-        cursor.insertRow([attributes[key] for key in attributes.keys()] + [json.dumps(feature['geometry'])])
+        cursor.insertRow([attributes[key] for key in list(attributes.keys())] + [json.dumps(feature['geometry'])])
 
 
 data = json.loads(arcpy.GetParameterAsText(0))
 # data = json.loads(open('TestData/NewCollectionEventData.json').read())
+# data = json.loads(open('TestData/NewCollectionEventData2.json').read())
+# data = json.loads(open('TestData/NewCollectionEventData3.json').read())
 arcpy.AddMessage('Received Data: {}'.format(data))
-
 
 arcpy.env.workspace = settings.DB
 
@@ -58,12 +75,17 @@ edit.startEditing(False, True)
 edit.startOperation()
 
 try:
-    for table in data.keys():
+    versioned = arcpy.Describe(list(data.keys())[0]).isVersioned
+    if not versioned:
+        arcpy.AddError('Data is versioned: {}'.format(versioned))
+
+    for table in list(data.keys()):
         if table == settings.SAMPLINGEVENTS:
             appendFeatureData(data[table])
         elif len(data[table]) > 0:
             appendTableData(table, data[table])
 except Exception as e:
+    arcpy.AddError(e)
     edit.abortOperation()
     edit.stopEditing(False)
     raise e
