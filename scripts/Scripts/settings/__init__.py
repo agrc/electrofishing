8a137f0540a582cbcb04dfa48813366362623f9a
Scott Davis <stdavis@utah.gov>
2019-10-23 12:27:23 -0600

    fix: fix path to streams data in gp tools #195

--- scripts/Scripts/settings/__init__.py
+++ scripts/Scripts/settings/__init__.py
@@ -2,15 +2,16 @@ from os import path
 
 SAMPLINGEVENTS = 'SamplingEvents'
 EVENT_DATE = 'EVENT_DATE'
-STREAMS = path.join(path.dirname(__file__), '..', '..', '..', 'data', 'dwrlakesstreams.gdb', 'Streams')
 
 # BEGIN LOCAL CONFIGS
+STREAMS = path.join(path.dirname(__file__), r'Electrofishing_LOCAL as WILDADMIN.sde\Electrofishing.WILDADMIN.UDWRStreams')
 STATIONS = path.join(path.dirname(__file__), r'Electrofishing_LOCAL as WILDADMIN.sde\Electrofishing.WILDADMIN.Stations')
 DB = path.join(path.dirname(__file__), r'Electrofishing_LOCAL as WILDADMIN.sde')
 DATAPATH = path.join(DB, 'Electrofishing.WILDADMIN.')
 # END LOCAL CONFIGS
 
 # BEGIN TEST SERVER CONFIGS
+# STREAMS = path.join(path.dirname(__file__), r'Electrofishing_TEST as WILDADMIN.sde\Wildlife.WILDADMIN.UDWRStreams')
 # STATIONS = path.join(path.dirname(__file__), r'ELECTROFISHING_TEST as WildAdmin.sde\Wildlife.WILDADMIN.Stations')
 # DB = path.join(path.dirname(__file__), r'ELECTROFISHING_TEST as WildAdmin.sde')
 # DATAPATH = path.join(DB, 'Wildlife.WILDADMIN.')
