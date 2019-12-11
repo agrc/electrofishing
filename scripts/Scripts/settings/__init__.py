a0a643cbff45430dd287ba20fb7e350ea3861920
Scott Davis <stdavis@utah.gov>
2019-12-11 10:32:53 -0700

    chore: tweaks for production deploy

--- scripts/Scripts/settings/__init__.py
+++ scripts/Scripts/settings/__init__.py
@@ -4,19 +4,22 @@ SAMPLINGEVENTS = 'SamplingEvents'
 EVENT_DATE = 'EVENT_DATE'
 
 # BEGIN LOCAL CONFIGS
-STREAMS = path.join(path.dirname(__file__), r'Electrofishing_LOCAL as WILDADMIN.sde\Electrofishing.WILDADMIN.UDWRStreams')
-STATIONS = path.join(path.dirname(__file__), r'Electrofishing_LOCAL as WILDADMIN.sde\Electrofishing.WILDADMIN.Stations')
-DB = path.join(path.dirname(__file__), r'Electrofishing_LOCAL as WILDADMIN.sde')
-DATAPATH = path.join(DB, 'Electrofishing.WILDADMIN.')
+# STREAMS = path.join(path.dirname(__file__), r'Electrofishing_LOCAL as WILDADMIN.sde\Electrofishing.WILDADMIN.UDWRStreams')
+# STATIONS = path.join(path.dirname(__file__), r'Electrofishing_LOCAL as WILDADMIN.sde\Electrofishing.WILDADMIN.Stations')
+# DB = path.join(path.dirname(__file__), r'Electrofishing_LOCAL as WILDADMIN.sde')
+# DATAPATH = path.join(DB, 'Electrofishing.WILDADMIN.')
 # END LOCAL CONFIGS
 
 # BEGIN TEST SERVER CONFIGS
-# STREAMS = path.join(path.dirname(__file__), r'Electrofishing_TEST as WILDADMIN.sde\Wildlife.WILDADMIN.UDWRStreams')
-# STATIONS = path.join(path.dirname(__file__), r'ELECTROFISHING_TEST as WildAdmin.sde\Wildlife.WILDADMIN.Stations')
-# DB = path.join(path.dirname(__file__), r'ELECTROFISHING_TEST as WildAdmin.sde')
-# DATAPATH = path.join(DB, 'Wildlife.WILDADMIN.')
+STREAMS = path.join(path.dirname(__file__), r'ELECTROFISHING_Test as WildAdmin.sde\Wildlife.WILDADMIN.UDWRStreams')
+STATIONS = path.join(path.dirname(__file__), r'ELECTROFISHING_Test as WildAdmin.sde\Wildlife.WILDADMIN.Stations')
+DB = path.join(path.dirname(__file__), r'ELECTROFISHING_Test as WildAdmin.sde')
+DATAPATH = path.join(DB, 'Wildlife.WILDADMIN.')
 # END TEST SERVER CONFIGS
 
 # BEGIN PROD SERVER CONFIGS
-# stations = r'Z:\Documents\Projects\Wildlife\dataentry\scripts\Scripts\UDWRAquatics as AquaticsEdit.sde\UDWRAquatics.AQUATICSADMIN.Stations'
+STREAMS = path.join(path.dirname(__file__), r'ELECTROFISHING_Prod as WildAdmin.sde\Electrofishing.WILDADMIN.UDWRStreams')
+STATIONS = path.join(path.dirname(__file__), r'ELECTROFISHING_Prod as WildAdmin.sde\Electrofishing.WILDADMIN.Stations')
+DB = path.join(path.dirname(__file__), r'ELECTROFISHING_Prod as WildAdmin.sde')
+DATAPATH = path.join(DB, 'Electrofishing.WILDADMIN.')
 # END PROD SERVER CONFIGS
