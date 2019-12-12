80f94450b76bfb1ec703a86621e0b15e0f27aee0
Scott Davis <stdavis@utah.gov>
2019-12-12 14:56:31 -0700

    Release v1.9.3

--- _src/app/config.js
+++ _src/app/config.js
@@ -61,7 +61,7 @@ define([
 
         // version: String
         //      The app version number.
-        version: '1.9.2',
+        version: '1.9.3',
 
         // coordTypes: {key:String}
         //      Coordinate types as used in app/SettingsDialog
