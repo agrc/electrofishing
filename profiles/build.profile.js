171f15f4fa54907d278b24d98bd11b58a93d5cb7
stdavis <stdavis@utah.gov>
2020-11-05 12:21:39 -0700

    fix: add css (via sass) solution for react components

--- profiles/build.profile.js
+++ profiles/build.profile.js
@@ -86,7 +86,7 @@ var profile = {
     }, {
         name: 'react-app',
         resourceTags: {
-            amd: () => true
+            amd: (filename) => /\.js/.test(filename)
         }
     }],
     staticHasFeatures: {
