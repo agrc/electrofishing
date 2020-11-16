b32be9c899ef15040225acfeeb23828f834540ca
stdavis <stdavis@utah.gov>
2020-11-16 14:41:32 -0700

    fix: make header more responsive
    Prevent wrapping content.

--- _src/react-app/components/Header.js
+++ _src/react-app/components/Header.js
@@ -5,65 +5,80 @@ import topic from 'pubsub-js';
 export default function Header() {
   return (
     <div className="navbar navbar-inverse navbar-fixed-top header">
-      <div className="container">
-        <span className="navbar-brand">
-          Electro-Fishing Data Collection
-        </span>
-        <a className="version pull-left" href="ChangeLog.html" target="_blank">
-          {config.version}
-        </a>
-        <ul className="nav navbar-nav">
-          <li className="active">
-            <a href="#locationTab" data-toggle="tab">
-              Location
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbar-collapse"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <span className="navbar-brand">
+            Electro-Fishing Data Collection
+            <a className="version" href="ChangeLog.html" target="_blank">
+              {config.version}
             </a>
-          </li>
-          <li>
-            <a href="#methodTab" data-toggle="tab">
-              Method
-            </a>
-          </li>
-          <li>
-            <a href="#catchTab" data-toggle="tab">
-              Catch
-            </a>
-          </li>
-          <li>
-            <a href="#habitatTab" data-toggle="tab">
-              Habitat
-            </a>
-          </li>
-        </ul>
-        <button
-          onClick={() => topic.publishSync(config.topics.onSubmitReportClick)}
-          data-loading-text="submitting report..."
-          className="btn btn-success navbar-btn"
-        >
-          Submit Report
-        </button>
-        <button
-          onClick={() => topic.publishSync(config.topics.onCancelReportClick)}
-          className="btn btn-default navbar-btn"
-        >
-          Cancel
-        </button>
-        <ul className="nav navbar-nav navbar-right">
-          <li>
-            <a href="../query" target="_blank">
-              Search
-            </a>
-          </li>
-          <li>
-            <a
-              role="button"
-              data-toggle="modal"
-              href="#settingsModal"
-              className="btn btn-small pull-right"
-            >
-              Settings <i className="icon-wrench"></i>
-            </a>
-          </li>
-        </ul>
+          </span>
+          <button
+            onClick={() =>
+              topic.publishSync(config.topics.onSubmitReportClick)
+            }
+            data-loading-text="submitting report..."
+            className="btn btn-success navbar-btn"
+          >
+            Submit Report
+          </button>
+          <button
+            onClick={() =>
+              topic.publishSync(config.topics.onCancelReportClick)
+            }
+            className="btn btn-default navbar-btn"
+          >
+            Cancel
+          </button>
+        </div>
+        <div className="collapse navbar-collapse" id="navbar-collapse">
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <a href="#locationTab" data-toggle="tab">
+                Location
+              </a>
+            </li>
+            <li>
+              <a href="#methodTab" data-toggle="tab">
+                Method
+              </a>
+            </li>
+            <li>
+              <a href="#catchTab" data-toggle="tab">
+                Catch
+              </a>
+            </li>
+            <li>
+              <a href="#habitatTab" data-toggle="tab">
+                Habitat
+              </a>
+            </li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="../query" target="_blank">
+                Search
+              </a>
+            </li>
+            <li>
+              <a role="button" data-toggle="modal" href="#settingsModal">
+                Settings <i className="icon-wrench"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
