0c6cb2709ca5c49def66cf7b342fb2cadb8a0534
stdavis <stdavis@utah.gov>
2021-03-04 16:43:16 -0700

    fix: simplify useDojoWidget api

--- _src/react-app/hooks/useDojoWidget.js
+++ _src/react-app/hooks/useDojoWidget.js
@@ -1,17 +1,23 @@
 import * as React from 'react';
 
-export default function useDojoWidget(widgetRef, nodeRef, WidgetClass) {
+export default function useDojoWidget(nodeRef, WidgetClass) {
+  const [widget, setWidget] = React.useState(null);
+
   React.useEffect(() => {
     if (!nodeRef.current) {
       return;
     }
 
-    widgetRef.current = new WidgetClass(null, nodeRef.current);
-    widgetRef.current.startup();
+    const newWidget = new WidgetClass(null, nodeRef.current);
+    newWidget.startup();
+
+    setWidget(newWidget);
 
     // destroy widgets when the parent component is destroyed
     return () => {
-      widgetRef.current.destroy();
+      newWidget.destroy();
     };
-  }, [widgetRef, nodeRef, WidgetClass]);
+  }, [nodeRef, WidgetClass]);
+
+  return widget;
 }
