0652d29147f2dc10f6be2e9e520c0e8e56f6a008
stdavis <stdavis@utah.gov>
2021-05-11 13:20:49 -0600

    Release v2.0.0-0

--- _src/react-app/config.js
+++ _src/react-app/config.js
@@ -195,7 +195,7 @@ const config = {
 
   // version: String
   //      The app version number.
-  version: '1.9.5',
+  version: '2.0.0-0',
 
   // coordTypes: {key:String}
   //      Coordinate types as used in app/SettingsDialog
