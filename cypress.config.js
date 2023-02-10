dae1033a754ce42e1fb5f3c103d7ee6281fdd3c5
stdavis <stdavis@utah.gov>
2023-02-10 08:27:49 -0700

    ci: upload cypress videos

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 120000,
-  video: false,
+  video: true,
   chromeWebSecurity: false,
   e2e: {
     // We've imported your old cypress plugins here.
