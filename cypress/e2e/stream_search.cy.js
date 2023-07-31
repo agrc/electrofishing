22348fddb1caf7db89f2981345eadc50fef70549
stdavis <stdavis@utah.gov>
2023-07-31 16:37:16 -0600

    fix: cypress tests

--- cypress/e2e/stream_search.cy.js
+++ cypress/e2e/stream_search.cy.js
@@ -6,11 +6,11 @@ describe('StreamSearch', () => {
     commonIntercepts();
     loadAndSignIn();
 
-    cy.get('#downshift-1-input').type('stream');
-    cy.get('#downshift-1-item-0').click();
+    cy.findAllByTestId('stream-search').first().type('fish');
+    cy.get('[role=listbox] .first-cell').first().click();
 
     cy.get(
-      '[d="M570 287L631 280L658 264L676 237L668 217L628 176L622 144L614 121L598 121L570 108L547 107L524 114L509 122L497 137L473 157L464 215L462 260L464 284L491 290z"]'
+      '[d="M570 287L631 280L658 264L676 237L668 217L628 176L622 144L614 121L598 121L570 108L547 107L524 114L509 122L497 137L473 157L464 215L462 260L464 284L491 290z"]',
     ).should('be.visible');
   });
 });
