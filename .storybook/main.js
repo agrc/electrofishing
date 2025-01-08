747864c88e28be08c3cbeb1eb616714f80b7b66a
stdavis <stdavis@utah.gov>
2025-01-08 10:51:49 -0700

    deps: FY25 Q3 dependency bumps 🌲

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,21 @@
 const config = {
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
+    '@chromatic-com/storybook'
+  ],
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   framework: {
     name: '@storybook/react-vite',
     options: {}
   },
-  docs: {
-    autodocs: true
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
   }
 };
 export default config;
